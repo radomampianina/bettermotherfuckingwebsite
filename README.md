bd8f6c672169d00462d759950bd2f09f4492c629
radomampianina <randriampenitra@yahoo.com>
2018-09-28 21:41:09 +0300

    ajout popup

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # bettermotherfuckingwebsite
-MotherFuckingWebsite_avec_css
-#Voir le site sur https://radomampianina.github.io/bettermotherfuckingwebsite/
+lien du site https://radomampianina.github.io/bettermotherfuckingwebsite/
---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function(){document.getElementById('popup').style.display = 'block'},10000);
+function fermer(){document.getElementById('popup').style.display = 'none'};
+function redir(){window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'};
+function psyde(){
+    document.bgColor = "magenta";
+}
